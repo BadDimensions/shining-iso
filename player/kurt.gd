12d6810b154abe5c90c2476317b83afb31279093
Stuart <11183054+stuartwk@users.noreply.github.com>
2026-03-05 22:37:16 +0900

    enemy sprite changes depending on direction

--- player/kurt.gd
+++ player/kurt.gd
@@ -2,7 +2,11 @@ class_name Player extends CharacterBody2D
 
 var cardinal_direction : Vector2 = Vector2.DOWN
 
-var direction : Vector2 = Vector2.ZERO
+var direction : Vector2 = Vector2.ZERO:
+	set(new_direction):
+		direction = new_direction
+		UpdateFacing(new_direction)
+
 var last_direction: Vector2 = Vector2.DOWN
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var state_machine: PlayerStateMachine = $StateMachine
@@ -17,43 +21,37 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	direction = Input.get_vector("left", "right", "up", "down").normalized()
-	if direction.length() >= 0.1:
-		last_direction = direction
-	SetDirection()
-	pass
 
 func _physics_process(delta : float) -> void:
 	if state_machine.current_state:
 		state_machine.ChangeState(state_machine.current_state.Physics(delta))
 	move_and_slide()
-	
-func SetDirection() -> void:
-	if direction.length() < 0.1:
-		last_direction = direction
+
+func UpdateFacing(new_direction: Vector2) -> void:
+	if new_direction.length() < 0.1:
 		return
 
-	var anim_dir = AnimDirection()
+	last_direction = new_direction
 
 	# Flip only for side animation
-	if direction.x != 0:
-		sprite_2d.scale.x = -1 if direction.x < 0 else 1
+	if new_direction.x != 0:
+		sprite_2d.scale.x = -1 if new_direction.x < 0 else 1
 	else:
 		sprite_2d.scale.x = 1
 	# Update cardinal direction (if you still need it)
-	if abs(direction.x) > abs(direction.y):
-		cardinal_direction = Vector2.RIGHT if direction.x > 0 else Vector2.LEFT
+	if abs(new_direction.x) > abs(new_direction.y):
+		cardinal_direction = Vector2.RIGHT if new_direction.x > 0 else Vector2.LEFT
 	else:
-		cardinal_direction = Vector2.DOWN if direction.y > 0 else Vector2.UP
+		cardinal_direction = Vector2.DOWN if new_direction.y > 0 else Vector2.UP
 
 	DirectionChanged.emit(cardinal_direction)
 
 func UpdateAnimation(state: String) -> void:
-	animation_player.play(state + "_" + AnimDirection())
-	
-func AnimDirection() -> String:
-	var dir = direction
-	if dir.length() < 0.1: 
-		dir = last_direction  
+	animation_player.play(state + "_" + AnimDirection(direction))
+
+func AnimDirection(dir: Vector2) -> String:
+	if dir.length() < 0.1:
+		dir = last_direction
 
 	if dir.y < 0 and abs(dir.x) < 0.5:
 		return "up"
@@ -65,4 +63,3 @@ func AnimDirection() -> String:
 		return "diagonal_down"
 	else:
 		return "side"
-	
