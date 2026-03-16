7df23a633343c520146c722a7ef6451d1665c024
BadDimensions <victoryincolour@gmail.com>
2026-03-16 06:33:01 -0400

    scene transitions

--- globals/globallevelmanager.gd
+++ globals/globallevelmanager.gd
@@ -1,9 +1,46 @@
 extends Node
 
+signal level_load_started
+signal level_loaded
+
 var current_tilemap_bounds: Array[Vector2]
+var target_transition : String
+var position_offset : Vector2
+
 signal TileMapBoundsChanged(bounds: Array[Vector2])
 # Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	await get_tree().process_frame
+	level_loaded.emit()
+
 func ChangeTileMapBounds(bounds: Array[Vector2]) -> void:
 		current_tilemap_bounds = bounds
 		TileMapBoundsChanged.emit(bounds)
 		print("Bounds being set: ", bounds)
+
+
+func load_new_level(
+		level_path: String,
+		_target_transition : String,
+		_position_offset : Vector2
+) -> void:
+	
+	get_tree().paused = true
+	target_transition = _target_transition
+	position_offset = _position_offset
+	
+	await get_tree().process_frame
+	
+	level_load_started.emit()
+	
+	await get_tree().process_frame
+	
+	get_tree().change_scene_to_file(level_path)
+	
+	await get_tree().process_frame
+	
+	get_tree().paused = false
+	
+	await get_tree().process_frame
+	
+	level_loaded.emit()
--- globals/playerglobalmanager.gd
+++ globals/playerglobalmanager.gd
@@ -1,11 +1,47 @@
 extends Node
 
+const KURT = preload("res://player/kurt.tscn")
+
 var player: Player
-# Called when the node enters the scene tree for the first time.
+var player_spawned : bool = false
+
 func _ready() -> void:
-	pass # Replace with function body.
+	add_player_instance()
+	await get_tree().create_timer(0.2).timeout
+	player_spawned = true
 
+func add_player_instance() -> void:
+	var actors = get_tree().get_first_node_in_group("Actors")
+	if actors == null:
+		actors = Node2D.new()
+		actors.name = "Actors"
+		actors.y_sort_enabled = true
+		actors.add_to_group("Actors")
+		get_tree().current_scene.add_child(actors)
+	player = KURT.instantiate()
+	actors.add_child(player)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+#this will be the final version of this function, the above is for testing only
+#func add_player_instance() -> void:
+	#player = KURT.instantiate()
+	#var actors = get_tree().get_first_node_in_group("actors")
+	#actors.add_child(player)
+
+func set_player_position(_new_pos : Vector2) -> void:
+	player.global_position = _new_pos
 	pass
+
+func set_as_parent(_p: Node2D) -> void:
+	if player.get_parent():
+		player.get_parent().remove_child(player)
+		_p.add_child(player)
+
+func unparent_player(_p: Node2D) -> void:
+	if not player:
+		return
+	if player.get_parent():
+		_p.remove_child(player)
+
+
+
+	
